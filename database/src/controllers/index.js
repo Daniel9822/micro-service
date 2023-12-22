f1ae2260eafc89df962239cd2d2553db2943ce90
Daniel9822 <dhdany64@gmail.com>
2023-12-22 16:18:28 -0400

    update

--- database/src/controllers/index.js
+++ database/src/controllers/index.js
@@ -6,11 +6,24 @@ const {
   deleteCharacter,
   createCharacter
 } = require('./characters.ctr')
+const {
+  listAllFilms,
+  listFilm,
+  updateFilm,
+  deleteFilm,
+  createFilm
+} = require('./films.ctr')
 
 module.exports = {
   listCharacters: catchError(listCharacters),
   listOneCharacter: catchError(listOneCharacter),
   updateCharacter: catchError(updateCharacter),
   deleteCharacter: catchError(deleteCharacter),
-  createCharacter: catchError(createCharacter)
+  createCharacter: catchError(createCharacter),
+  //films
+  listAllFilms: catchError(listAllFilms),
+  listFilm: catchError(listFilm),
+  updateFilm: catchError(updateFilm),
+  deleteFilm: catchError(deleteFilm),
+  createFilm: catchError(createFilm)
 }
