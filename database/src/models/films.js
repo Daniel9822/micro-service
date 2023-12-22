03662f205efc3341a59e82e9f3a9bfd6f035ac96
Daniel9822 <dhdany64@gmail.com>
2023-12-22 15:14:38 -0400

    update

--- database/src/models/films.js
+++ database/src/models/films.js
@@ -1,4 +1,11 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose')
+const {
+  listItems,
+  insertItem,
+  listItem,
+  deleteItem,
+  updateItem
+} = require('./staticsMethods')
 
 const filmSchema = new Schema(
   {
@@ -7,13 +14,19 @@ const filmSchema = new Schema(
     director: String,
     producer: String,
     release_date: Date,
-    characters: [{ type: String, ref: "character" }],
-    planets: [{ type: String, ref: "planet" }]
+    characters: [{ type: String, ref: 'character' }],
+    planets: [{ type: String, ref: 'planet' }]
   },
   {
     timestamps: true
   }
-);
+)
+
+filmSchema.statics.list = async () => listItems(films)
+filmSchema.statics.insert = async (data) => insertItem(films, data)
+filmSchema.statics.getItemById = async (id) => listItem(films, id)
+filmSchema.statics.deleteItemById = async (id) => deleteItem(films, id)
+filmSchema.statics.updateItem = async (id, data) => updateItem(films, id, data)
 
 const films = model('film', filmSchema)
 
