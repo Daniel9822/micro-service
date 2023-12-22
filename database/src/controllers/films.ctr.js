2ffd76396d36e96e7025ae681b8b872991b72c83
Daniel9822 <dhdany64@gmail.com>
2023-12-22 16:18:03 -0400

    update

--- database/src/controllers/films.ctr.js
+++ database/src/controllers/films.ctr.js
@@ -1,23 +1,24 @@
+const response = require('../../helpers/response')
 const dataService = require('../services/data.service')
 const modelName = 'films'
 
 const listAllFilms = async (req, res) => {
   const allItems = await dataService.listData(modelName)
-  res.status(200).json(allItems)
+  response(res, 200, allItems)
 }
 
 const listFilm = async (req, res) => {
   const { id } = req.params
 
   const item = await dataService.getItemById(modelName, id)
-  res.status(200).send(item)
+  response(res, 200, item)
 }
 
 const createFilm = async (req, res) => {
   const body = req.body;
 
   const create = await dataService.insertData(modelName, body)
-  res.status(201).send(create)
+  response(res, 201, create)
 }
 
 const updateFilm = async (req, res) => {
@@ -25,14 +26,14 @@ const updateFilm = async (req, res) => {
   const body = req.body;
 
   const update = await dataService.updateItem(modelName, id, body)
-  res.status(200).send(update)
+  response(res, 200, update)
 }
 
 const deleteFilm = async (req, res) => {
   const { id } = req.params;
 
   const delFilm = await dataService.deleteItemById(modelName, id)
-  res.status(200).send(delFilm)
+  response(res, 200, delFilm)
 }
 
 
