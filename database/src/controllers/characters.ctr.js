6ed0f7ad3c9119548fcf46872f7ab70b2a5c6fd8
Daniel9822 <dhdany64@gmail.com>
2023-12-22 16:17:48 -0400

    update

--- database/src/controllers/characters.ctr.js
+++ database/src/controllers/characters.ctr.js
@@ -1,31 +1,33 @@
+const response = require('../../helpers/response')
 const dataService = require('../services/data.service')
 
 const modelName = 'characters'
 
 const listCharacters = async (req, res) => {
   const data = await dataService.listData(modelName)
-  res.status(200).json(data)
+  response(res, 200, data)
 }
 
 const createCharacter = async (req, res) => {
   const body = req.body
 
   const create = await dataService.insertData(modelName, body)
-  res.status(201).json(create)
+  response(res, 201, create)
 }
 
 const listOneCharacter = async (req, res) => {
   const { id } = req.params
 
   const item = await dataService.getItemById(modelName, id)
-  res.status(200).json(item)
+  response(res, 200, item)
+
 }
 
 const deleteCharacter = async (req, res) => {
   const { id } = req.params
 
   const remove = await dataService.deleteItemById(modelName, id)
-  res.status(200).send(remove)
+  response(res, 200, remove)
 }
 
 const updateCharacter = async (req, res) => {
@@ -33,7 +35,7 @@ const updateCharacter = async (req, res) => {
   const body = req.body
 
   const update = await dataService.updateItem(modelName, id, body)
-  res.status(200).json(update)
+  response(res, 200, update)
 }
 
 module.exports = {
