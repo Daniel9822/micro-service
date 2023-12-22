e89dd8dc78b8f821313d9488b0d9e86e9e1edaf2
Daniel9822 <dhdany64@gmail.com>
2023-12-22 18:15:02 -0400

    feat: helpers

---
+++ database/test/helpers/characters.js
@@ -0,0 +1,16 @@
+const createItem = {
+  name: 'Add test',
+  height: '178',
+  hair_color: 'blue',
+  skin_color: 'black',
+  eye_color: 'negro',
+  birth_year: '22',
+  gender: 'extraterreste',
+  films: [],
+}
+
+
+
+module.exports = {
+  createItem
+}
