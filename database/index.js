0dc3dfa3a704993b7931db650d6b309b26a1b6cb
Daniel9822 <dhdany64@gmail.com>
2023-12-22 16:17:08 -0400

    update

--- database/index.js
+++ database/index.js
@@ -2,7 +2,14 @@ require('dotenv').config()
 const connect = require('./src/config/db.config')
 const server = require('./src/server')
 
-server.listen(4001, async () => {
-  await connect()
-  console.log('db is listener on port 4001') 
-})
+const NODE_ENV = process.env.NODE_ENV
+
+if (NODE_ENV !== 'test') {
+  server.listen(4001, async () => {
+    console.log('db is listener on port 4001')
+  })
+}
+
+connect().then()
+
+module.exports = server
