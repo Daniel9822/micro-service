379f50a5f5e904eeec441d7150d8bccb42ecab40
Daniel9822 <dhdany64@gmail.com>
2023-12-22 15:15:07 -0400

    update

--- database/src/server.js
+++ database/src/server.js
@@ -7,4 +7,11 @@ server.use(express.json())
 
 server.use('/db', require('./routes'))
 
+server.use((err, req, res, next) => {
+  res.status(err?.statusCode || 500).send({
+    error: true,
+    message: err?.message
+  })
+})
+
 module.exports = server
