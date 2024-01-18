656617ad32c298f22c85112f5087e56c5dae0cd8
Daniel9822 <dhdany64@gmail.com>
2024-01-18 15:54:45 -0400

    update

--- characters/src/controllers/characters.ctr.js
+++ characters/src/controllers/characters.ctr.js
@@ -17,13 +17,28 @@ const updateCharacter = async (req, res) => {
   const { id } = req.params
   const body = req.body
 
-  console.log(body);
   const data = await DBServices.updateCharacter(id, body)
+  response(res, 200, data)
+}
+
+const createCharacter = async (req, res) => {
+  const body = req.body
+
+  const data = await DBServices.createCharacter(body)
+  response(res, 201, data)
+}
+
+const deleteCharacter = async (req, res) => {
+  const { id } = req.params
+
+  const data = await DBServices.deleteCharacter(id)
   response(res, 200, data)
 }
 
 module.exports = {
   getAllCharacters,
   getCharacter,
-  updateCharacter
+  updateCharacter,
+  createCharacter,
+  deleteCharacter
 }
