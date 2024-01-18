3911bb5183b7cf2b26f28dc4288007c81d34f855
Daniel9822 <dhdany64@gmail.com>
2024-01-18 19:35:07 -0400

    update

--- characters/src/controllers/index.js
+++ characters/src/controllers/index.js
@@ -1,10 +1,16 @@
 const catchError = require('../utils/catchError')
-const { getAllCharacters, getCharacter, updateCharacter } = require('./characters.ctr')
-
-
+const {
+  getAllCharacters,
+  getCharacter,
+  updateCharacter,
+  deleteCharacter,
+  createCharacter
+} = require('./characters.ctr')
 
 module.exports = {
   getAllCharacters: catchError(getAllCharacters),
   getCharacter: catchError(getCharacter),
-  updateCharacter: catchError(updateCharacter)
+  updateCharacter: catchError(updateCharacter),
+  createCharacter: catchError(createCharacter),
+  deleteCharacter: catchError(deleteCharacter)
 }
