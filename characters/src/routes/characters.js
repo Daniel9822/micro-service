ae4bb4e23b9ab92b0d9726a70f0ebc22b52438c8
Daniel9822 <dhdany64@gmail.com>
2024-01-18 19:35:18 -0400

    update

--- characters/src/routes/characters.js
+++ characters/src/routes/characters.js
@@ -1,9 +1,12 @@
-const { getAllCharacters, getCharacter, updateCharacter } = require('../controllers')
+const { getAllCharacters, getCharacter, updateCharacter, createCharacter, deleteCharacter } = require('../controllers')
 
 const router = require('express').Router()
 
 router.get('/', getAllCharacters)
 router.get('/:id', getCharacter)
 router.put('/:id', updateCharacter)
+router.post('/', createCharacter)
+router.delete('/:id', deleteCharacter)
+
 
 module.exports = router
