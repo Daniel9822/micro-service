ead320b1514d9f0d2de8301c6bc12015a2b4fb3c
Daniel9822 <dhdany64@gmail.com>
2024-01-18 15:25:23 -0400

    update

--- characters/src/controllers/characters.ctr.js
+++ characters/src/controllers/characters.ctr.js
@@ -1,11 +1,29 @@
-const DbServices = require('../services/dbServices')
+const DBServices = require('../services/dbServices')
 const response = require('../utils/response')
 
 const getAllCharacters = async (req, res) => {
-  const data = await DbServices.allCahracter()
-  response(res, 200, data) 
+  const data = await DBServices.allCharacter()
+  response(res, 200, data)
+}
+
+const getCharacter = async (req, res) => {
+  const { id } = req.params
+
+  const data = await DBServices.getCharacterById(id)
+  response(res, 200, data)
+}
+
+const updateCharacter = async (req, res) => {
+  const { id } = req.params
+  const body = req.body
+
+  console.log(body);
+  const data = await DBServices.updateCharacter(id, body)
+  response(res, 200, data)
 }
 
 module.exports = {
-  getAllCharacters
+  getAllCharacters,
+  getCharacter,
+  updateCharacter
 }
