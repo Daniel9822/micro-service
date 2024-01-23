5e67087a8d53a8f2298ac0fd3eeea25d78b42ff6
Daniel9822 <dhdany64@gmail.com>
2024-01-23 16:16:26 -0400

    update

--- films/src/routes/films.js
+++ films/src/routes/films.js
@@ -1,11 +1,16 @@
 const router = require('express').Router()
-const { createFilm, getAllFilms } = require('../controllers')
-
+const {
+  createFilm,
+  getAllFilms,
+  updateFilms,
+  deleteFilm
+} = require('../controllers')
+const { getFilmById } = require('../controllers/films.ctr')
 
 router.get('/', getAllFilms)
 router.post('/', createFilm)
-router.get('/')
-router.put('/')
-router.delete('/')
+router.get('/:id', getFilmById)
+router.put('/:id', updateFilms)
+router.delete('/:id', deleteFilm)
 
 module.exports = router
