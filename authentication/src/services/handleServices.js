5321e62daf4a0e260b5e1ae04b335387ff40d465
Daniel9822 <dhdany64@gmail.com>
2024-01-23 21:18:11 -0400

    feat: authentication services

---
+++ authentication/src/services/handleServices.js
@@ -0,0 +1,29 @@
+const { DB_URL } = require('../utils/envs')
+
+class DbServices {
+  constructor() {}
+
+  async verifyUser(email) {
+    const response = await fetch(`${DB_URL}/${email}`, {
+      method: 'GET',
+    })
+
+    const { data } = await response.json()
+    return data
+  }
+  
+  async register(user) {
+    const response = await fetch(DB_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+
+    const { data } = response.json()
+    return data
+  }
+}
+
+module.exports = new DbServices()
