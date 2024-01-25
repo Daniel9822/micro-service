7f5e2b5f93dd7fda4a868c6ff79b64c42c58dc83
Daniel9822 <dhdany64@gmail.com>
2024-01-25 19:19:16 -0400

    update

--- database/src/server.js
+++ database/src/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const HandleError = require('../../characters/src/utils/handleError')
 
 const server = express()
 server.use(morgan('dev'))
 server.use(express.json())
 
 server.use('/db', require('./routes'))
+
+server.use('*', (req, res) => {
+  throw new HandleError('route not found', 404)
+})
 
 server.use((err, req, res, next) => {
   res.status(err?.statusCode || 500).send({
