faabb00fe43a764b4b1a7e57160022ae29e97025
Daniel9822 <dhdany64@gmail.com>
2024-01-25 19:21:41 -0400

    update

--- authentication/src/controllers/auth.ctr.js
+++ authentication/src/controllers/auth.ctr.js
@@ -1,8 +1,67 @@
-const registerUser = async (req, res) => {}
+const response = require('../utils/response')
+const DbServices = require('../services/handleServices')
+const { matchedData } = require('express-validator')
+const { hashPassword, comparePassword } = require('../utils/bcrypt')
+const HandleError = require('../utils/handleError')
+const { generateToken, decodeToken } = require('../jwt/jwt')
 
-const loginUser = async (req, res) => {}
+const registerUser = async (req, res) => {
+  const body = matchedData(req)
+
+  const hash = await hashPassword(body.password)
+
+  const { data } = await DbServices.register({
+    email: body.email,
+    password: hash
+  })
+
+  response(res, 201, data)
+}
+
+const loginUser = async (req, res) => {
+  const { password, email } = matchedData(req)
+
+  const user = await DbServices.verifyUser(email)
+
+  if (!user?._id) {
+    throw new HandleError('Invalid user', 401)
+  }
+
+  const compare = await comparePassword(password, user.password)
+
+  if (!compare || compare.error) {
+    throw new HandleError('Unauthorized user', 401)
+  }
+
+  const token = generateToken(user)
+
+  response(res, 200, {
+    user: {
+      email: user.email,
+      token
+    }
+  })
+}
+
+const authorization = async (req, res) => {
+  const { token } = req.body
+
+  const decode = decodeToken(token)
+
+  if (decode?.email) {
+    response(res, 200, {
+      authorization: true
+    })
+  } else {
+    response(res, 401, {
+      error: true,
+      message: 'unauthorized user'
+    })
+  }
+}
 
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  authorization
 }
