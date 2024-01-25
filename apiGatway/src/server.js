09c7377c75e42f1db87830dcd2a3fad8a6fd2841
Daniel9822 <dhdany64@gmail.com>
2024-01-25 19:20:37 -0400

    update

--- apiGatway/src/server.js
+++ apiGatway/src/server.js
@@ -1,29 +1,48 @@
 const express = require('express')
 const morgan = require('morgan')
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const proxyReq = require('./utils/onRequest');
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const proxyReq = require('./utils/onRequest')
+const authorization = require('./middleware/authorization')
 
 const server = express()
 
 server.use(morgan('dev'))
 
-server.use('/characters', createProxyMiddleware({
-  target: 'http://localhost:3000',
-  changeOrigin: true,
-  onProxyReq: proxyReq
-}))
+server.use(
+  '/auth/:action(login | register)?', 
+  createProxyMiddleware({
+    target: 'http://localhost:5000',
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 
-server.use('films', createProxyMiddleware({
-  target: 'http://localhost:3001',
-  changeOrigin: true,
-  onProxyReq: proxyReq
-}))
+server.use(
+  '/characters',
+  createProxyMiddleware({
+    target: 'http://localhost:3000',
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 
-server.use('planets', createProxyMiddleware({
-  target: 'http://localhost:3002',
-  changeOrigin: true,
-  onProxyReq: proxyReq
-}))
+server.use(
+  '/films',
+  authorization,
+  createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 
+server.use(
+  '/planets',
+  createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 
 module.exports = server
