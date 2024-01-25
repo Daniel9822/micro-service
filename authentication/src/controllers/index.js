54c0452ba9963ae9ff095d5d51ef197bd7245d64
Daniel9822 <dhdany64@gmail.com>
2024-01-25 19:21:54 -0400

    update

--- authentication/src/controllers/index.js
+++ authentication/src/controllers/index.js
@@ -1,7 +1,8 @@
 const catchError = require('../utils/catchError')
-const { registerUser, loginUser } = require('./auth.ctr') 
+const { registerUser, loginUser, authorization } = require('./auth.ctr') 
 
 module.exports = {
   registerUser: catchError(registerUser),
-  loginUser: catchError(loginUser)
+  loginUser: catchError(loginUser),
+  authorization: catchError(authorization)
 }
