cbc24370898375199c6133bdab3a246ffee0ea7e
Daniel9822 <dhdany64@gmail.com>
2024-01-25 19:20:51 -0400

    update

--- authentication/index.js
+++ authentication/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/server')
 const { PORT } = require('./src/utils/envs')
-
+ 
 server.listen(PORT || 5000, () => {
   console.log(`server on listener on port ${PORT}`)
 })
