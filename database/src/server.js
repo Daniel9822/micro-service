fda4ef474d7943ee66c4a4e2f599b56a7741045d
Daniel9822 <dhdany64@gmail.com>
2024-02-06 16:49:29 -0400

    upate

--- database/src/server.js
+++ database/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-const HandleError = require('../../characters/src/utils/handleError')
+const HandleError = require('../helpers/errorHandler')
 
 const server = express()
 server.use(morgan('dev'))
