4783ab61aef643501d7494a7b16548d304673ad0
Daniel9822 <dhdany64@gmail.com>
2024-02-06 16:48:53 -0400

    update

--- characters/src/utils/envs.js
+++ characters/src/utils/envs.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const NODE_ENV = process.env.NODE_ENV
 module.exports = {
-  DB_URI: process.env.DB_URI
+  DB_URI: NODE_ENV === 'development' ? process.env.DB_URL_LOCAL : process.env.DB_URI
 }
