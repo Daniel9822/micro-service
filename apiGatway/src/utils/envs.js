f911531d3f89517273db3dd2ddc90cd8af82831b
Daniel9822 <dhdany64@gmail.com>
2024-02-06 16:47:10 -0400

    update

--- apiGatway/src/utils/envs.js
+++ apiGatway/src/utils/envs.js
@@ -1,6 +1,21 @@
 require('dotenv').config()
 
-module.exports = {
-  secret: process.env.SECRET,
-  authorization_url: process.env.AUTHORIZATION_URL
+const NODE_ENV = process.env.NODE_ENV
+
+const services = {
+  authorization_url: process.env.AUTHORIZATION_URL,
+  authorization: process.env.AUTHORIZATION,
+  characters: process.env.CHARACTERS,
+  planets: process.env.PLANETS,
+  films: process.env.FILMS
 }
+
+console.log(services);
+
+if (NODE_ENV === "development") {
+  for (const key in services) {
+    services[key] = services[key].replace(/\/\/[^:]+:/, "//localhost:");
+  }
+}
+
+module.exports = services
