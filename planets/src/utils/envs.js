f8ac74a75856e8391fe898b0a8d86a65dfc3ff1e
Daniel9822 <dhdany64@gmail.com>
2024-02-06 16:50:59 -0400

    update

--- planets/src/utils/envs.js
+++ planets/src/utils/envs.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
+const NODE_ENV = process.env.NODE_ENV
+
 module.exports = {
-  DB_URL: process.env.DB_URL,
+  DB_URL:
+    NODE_ENV === 'development' ? process.env.DB_URL_LOCAL : process.env.DB_URL,
   PORT: process.env.PORT
 }
