d8ba44103f52eb06ed20abc785d458e54b5c096e
Daniel9822 <dhdany64@gmail.com>
2024-02-06 16:48:18 -0400

    update

--- authentication/src/utils/envs.js
+++ authentication/src/utils/envs.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
+const NODE_ENV = process.env.NODE_ENV
+
 module.exports = {
   secret: process.env.SECRET,
   PORT: process.env.PORT,
-  DB_URL: process.env.DB_URL
+  DB_URL:
+    NODE_ENV === 'development' ? process.env.DB_URL_LOCAL : process.env.DB_URL
 }
